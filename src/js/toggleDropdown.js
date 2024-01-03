727484b595af2f96a5b9fc542506929cbe0a72ee
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-01-03 22:59:58 +0200

    style decomposition, css variables

--- src/js/toggleDropdown.js
+++ src/js/toggleDropdown.js
@@ -2,9 +2,9 @@ function toggleDropdown() {
     var dropdownContent = document.getElementById('dropdown-content');
     dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
 
-    var dropdownBtn = document.querySelector('.header-dropdown-btn');
+    var dropdownBtn = document.querySelector('.header-dropdown-button');
 
     dropdownBtn.classList.toggle('rotated')
 }
 
-document.querySelector('.header-dropdown-btn').addEventListener('click', toggleDropdown)
+document.querySelector('.header-dropdown-button').addEventListener('click', toggleDropdown)
